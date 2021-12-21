123904d857ee6a00166ecdeb6cb283283c39016d
asadbek <asadbek30665@gmail.com>
2021-12-21 09:20:52 +0500

    run migrations database

---
+++ scripts/run-migration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export POSTGRES_USER=asadbek
+export POSTGRES_PASSWORD=3066586
+export POSTGRES_DATABASE=tododb
+
+
+migrate -database "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DATABASE?sslmode=disable" -path "./migrations"  up
